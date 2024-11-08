1bff4a04b2d673c5d9c09b529b1231f969384dbf
Axello111 <chernyxnekit@gmail.com>
2024-11-08 16:07:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+print(9 ** 0.5 * 5) #1sr program
+
+print(9.99 > 9.98 and 1000 != 100.1) #2nd program
+
+print(2*2+2)  #3rd program
+print(2*(2+2))
+print(2*2+2 == 2*(2+2))
+
+print(int((float('123.456')) * 10) % 10)  #4th program
